8eb95b6faf26fdb8fde4c784f03ecdc8b8bb88be
Doctor-san <adrian.rene.sanch@gmail.com>
2023-12-21 10:28:19 -0700

    refactored and added delete method

--- app.js
+++ app.js
@@ -7,39 +7,58 @@ const port = process.env.PORT || 8002;
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/bookmarks', (req, res) => {
-  pool.query('SELECT * FROM bookmarks')
-    .then((data) => res.status(200).send(data.rows))
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Sorry!');
-    })
+app.get('/bookmarks', async (req, res) => {
+  try {
+    const data = await pool.query('SELECT * FROM bookmarks');
+    res.status(200).send(data.rows);
+  }
+  catch (error) {
+    res.status(500).send('Sorry');
+  }
 });
 
-app.post('/bookmarks', (req, res) => {
+app.post('/bookmarks', async (req, res) => {
   const { title, url, description, topic_id } = req.body;
-  pool.query(
-    'INSERT INTO bookmarks (title, url, description, topic_id) VALUES ($1, $2, $3, $4)',
-    [title, url, description, topic_id]
-  ).then(() => res.status(201).send('Bookmark created successfully'))
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Sorry!');
-    });
+  try {
+    await pool.query(
+      'INSERT INTO bookmarks (title, url, description, topic_id) VALUES ($1, $2, $3, $4)',
+      [title, url, description, topic_id]
+    );
+    res.status(201).send('Bookmark created successfully');
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Sorry!');
+  }
 });
 
-app.patch('/bookmarks/:id', (req, res) => {
+app.patch('/bookmarks/:id', async (req, res) => {
   const bookmarkId = req.params.id;
   const { title, url, description, topic_id } = req.body;
-  pool.query(
-    'UPDATE bookmarks SET title = $1, url = $2, description = $3, topic_id = $4 WHERE id = $5',
-    [title, url, description, topic_id, bookmarkId]
-  ).then(() => res.send(`Bookmark with ID ${bookmarkId} updated successfully`))
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Sorry');
-    });
+  try {
+    await pool.query(
+      'UPDATE bookmarks SET title = $1, url = $2, description = $3, topic_id = $4 WHERE id = $5',
+      [title, url, description, topic_id, bookmarkId]
+    );
+    res.send(`Bookmark with ID ${bookmarkId} updated successfully`);
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Sorry');
+  }
 });
+
+app.delete('/bookmarks/:id', async (req, res) => {
+  const bookmarkId = req.params.id;
+  try {
+    await pool.query('DELETE FROM bookmarks WHERE id = $1', [bookmarkId]);
+    res.send(`Bookmark with ID ${bookmarkId} deleted successfully`);
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).send('Sorry');
+  }
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
